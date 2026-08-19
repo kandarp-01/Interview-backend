22a3d03a7279e7a7ec80d487f7cd54ed8c99f61a
Kandarp Dubey <kandarp1415@gmail.com>
2026-08-19 12:42:44 +0530

    update production cors

--- src/app.js
+++ src/app.js
@@ -7,10 +7,17 @@ const interviewRouter = require("./routes/interview.routes")
 const app = express();
 
 app.use(express.json()); 
-app.use(cors({
-    origin:"http://localhost:5173",
-    credentials:true
-}))
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://interview-frontend-sable.vercel.app",
+];
+
+app.use(
+  cors({
+    origin: allowedOrigins,
+    credentials: true,
+  })
+);
 app.use(cookieParser());
 app.use("/api/auth",authRouter);
 app.use("/api/interview",interviewRouter);
