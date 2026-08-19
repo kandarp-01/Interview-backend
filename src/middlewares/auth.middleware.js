65d9bcae05134c46b62f8bc895b89f16f997d22b
Kandarp Dubey <kandarp1415@gmail.com>
2026-08-19 12:05:59 +0530

    testing

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -4,14 +4,14 @@ const jwt=require("jsonwebtoken");
 async function authUser(req,res,next){
     const token=req.cookies.token;
     if(!token){
-        return res.status(403).json({
+        return res.status(401).json({
             message:"Token is missing"
         })
     }
     const decoded=jwt.verify(token,process.env.JWT_SECRET);
     const session=await redisClient.HGETALL(`session:${decoded.jti}`);
     if(Object.keys(session).length===0){
-        return res.status(403).json({
+        return res.status(401).json({
             message:"Unauthorized"
         })
     }
